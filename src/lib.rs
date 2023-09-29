e2c8ff4e0050b3914fbc00ea65d34556105769d5
Steven Salaun <steven.salaun@gmail.com>
2023-09-29 18:26:18 +0200

    fix version bump & update cli

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ pub enum Error {
     #[error("An error occurred during version parsing")]
     SemverParseError(#[from] semver::Error),
     /// An error that occurred during the toml parsing.
-    #[error("a parser error occurred")]
+    #[error("a toml parser error occurred")]
     ParseError(#[from] TomlError),
     /// An error that gets emitted if the `package.version` field has not the
     /// right type (String).
@@ -24,13 +24,29 @@ pub enum Error {
 
 /// An enum defining what types of increments can be done to a semver version.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-pub enum SemVer {
+pub enum Increment {
     /// A major increment.
     Major,
     /// A minor increment.
     Minor,
     /// A patch increment.
     Patch,
+}
+
+pub fn get_package_version_str(path: impl AsRef<Path>) -> Result<String, Error> {
+    let cargo_toml_content = fs::read_to_string(path.as_ref())?;
+    let doc = cargo_toml_content.parse::<Document>()?;
+    let item: &Item = &doc["package"]["version"];
+
+    // This should be the case for valid Cargo.toml files.
+    if let Some(s) = item.as_str() {
+        Ok(s.to_string())
+    } else {
+        Err(Error::InvalidFieldType {
+            field: "version".to_string(),
+            ty: "string".to_string(),
+        })
+    }
 }
 
 /// Returns the version inside a `Cargo.toml` file.
@@ -70,14 +86,15 @@ pub fn get_version(path: impl AsRef<Path>) -> Result<Version, Error> {
 /// # Returns
 ///
 /// An error if something went wrong during IO operations or parsing.
-pub fn set_version(path: impl AsRef<Path>, version: impl AsRef<str>) -> Result<(), Error> {
+pub fn set_version(path: impl AsRef<Path>, version_str: impl AsRef<str>) -> Result<Version, Error> {
+    let version = Version::parse(version_str.as_ref())?;
     let cargo_toml_content = fs::read_to_string(path.as_ref())?;
     let mut doc = cargo_toml_content.parse::<Document>()?;
 
-    doc["package"]["version"] = value(version.as_ref());
+    doc["package"]["version"] = value(&version.to_string());
     fs::write(path.as_ref(), doc.to_string())?;
 
-    Ok(())
+    Ok(version)
 }
 
 /// Bumps the version inside a `Cargo.toml` file according to semver specs.
@@ -90,15 +107,20 @@ pub fn set_version(path: impl AsRef<Path>, version: impl AsRef<str>) -> Result<(
 /// # Returns
 ///
 /// The new version or an error if something went wrong during IO operations.
-pub fn bump_version(path: impl AsRef<Path>, r#type: SemVer) -> Result<Version, Error> {
-    let mut version = get_version(path.as_ref())?;
-    match r#type {
-        SemVer::Major => version.increment_major(),
-        SemVer::Minor => version.increment_minor(),
-        SemVer::Patch => version.increment_patch(),
-    }
-
+pub fn bump_toml_version(path: impl AsRef<Path>, increment: Increment) -> Result<Version, Error> {
+    let version_str = get_package_version_str(path.as_ref())?;
+    let version = bump_version(&version_str, increment)?;
     set_version(path, &version.to_string())?;
+    Ok(version)
+}
+
+pub fn bump_version(version_str: &str, increment: Increment) -> Result<Version, Error> {
+    let mut version: Version = Version::parse(version_str)?;
+    match increment {
+        Increment::Major => version.bump_major(),
+        Increment::Minor => version.bump_minor(),
+        Increment::Patch => version.bump_patch(),
+    }
     Ok(version)
 }
 
@@ -106,6 +128,10 @@ trait SemVerExt {
     fn increment_major(&mut self);
     fn increment_minor(&mut self);
     fn increment_patch(&mut self);
+
+    fn bump_major(&mut self);
+    fn bump_minor(&mut self);
+    fn bump_patch(&mut self);
 }
 
 impl SemVerExt for Version {
@@ -119,5 +145,43 @@ impl SemVerExt for Version {
 
     fn increment_patch(&mut self) {
         self.patch += 1;
+    }
+
+    fn bump_major(&mut self) {
+        self.major += 1;
+        self.minor = 0;
+        self.patch = 0;
+    }
+
+    fn bump_minor(&mut self) {
+        self.minor += 1;
+        self.patch = 0;
+    }
+
+    fn bump_patch(&mut self) {
+        self.patch += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{bump_version, Increment};
+
+    #[test]
+    fn test_version_bump() {
+        const BASE_VERSION: &str = "0.1.0";
+        let mut v = bump_version(BASE_VERSION, Increment::Patch).unwrap();
+        assert_eq!(&v.to_string(), "0.1.1");
+        v = bump_version(&v.to_string(), Increment::Minor).unwrap();
+        assert_eq!(&v.to_string(), "0.2.0");
+        v = bump_version(&v.to_string(), Increment::Patch).unwrap();
+        v = bump_version(&v.to_string(), Increment::Patch).unwrap();
+        assert_eq!(&v.to_string(), "0.2.2");
+        v = bump_version(&v.to_string(), Increment::Major).unwrap();
+        assert_eq!(&v.to_string(), "1.0.0");
+        v = bump_version(&v.to_string(), Increment::Minor).unwrap();
+        assert_eq!(&v.to_string(), "1.1.0");
+        v = bump_version(&v.to_string(), Increment::Patch).unwrap();
+        assert_eq!(&v.to_string(), "1.1.1");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,38 @@
-use cargo_next::{bump_version, get_version, set_version, SemVer};
-use clap::Parser;
+use cargo_next::{bump_toml_version, get_version, set_version, Increment};
+use clap::{Parser, Subcommand};
+use semver::Version;
 use std::{env::current_dir, io, process::exit};
 
 #[derive(Debug, Parser)]
-#[clap(
-    author,
-    bin_name("cargo-next"),
-    version
-)]
-enum Cli {
-    #[clap(name = "next")]
-    Next(Args),
+#[clap(author, bin_name("cargo-next"), version)]
+struct Cli {
+    /// This is because when we're called from cargo, our first arg is the command we were calld as.
+    next: String,
+    #[command(subcommand)]
+    command: Commands,
 }
 
-#[derive(Debug, Parser)]
-struct Args {
-    /// Returns the current version of a crate.
-    #[clap(long)]
-    pub get: bool,
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Get,
+    Major,
+    Minor,
+    Patch,
+    Set { version: Option<String> },
+}
 
-    /// Increment the crate's major version.
-    #[clap(long)]
-    pub major: bool,
-
-    /// Increment the crate's minor version.
-    #[clap(long)]
-    pub minor: bool,
-
-    /// Increment the crate's patch version.
-    #[clap(long)]
-    pub patch: bool,
-
-    /// The version to set the crate to.
-    pub version: Option<String>,
+fn read_stdin() -> Result<Option<String>, std::io::Error> {
+    let mut piped = String::new();
+    io::stdin().read_line(&mut piped)?;
+    let piped_trim = piped.trim();
+    match piped_trim.is_empty() {
+        true => Ok(None),
+        false => Ok(Some(piped_trim.to_string())),
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
-    let mut cli = match cli {
-        Cli::Next(args) => args,
-    };
-
-    // If no flag has been specified and no version, read from stdin.
-    if !cli.major && !cli.minor && !cli.patch && !cli.get && cli.version.is_none() {
-        let mut piped = String::new();
-        io::stdin().read_line(&mut piped)?;
-        let piped_trim = piped.trim();
-        if !piped_trim.is_empty() {
-            cli.version = Some(piped_trim.to_string());
-        }
-    }
 
     // Check if the current directory is actually a cargo project.
     let cargo_project_dir_path = current_dir()?;
@@ -59,19 +42,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         exit(1);
     }
 
-    if cli.get {
-        println!("{}", get_version(&cargo_toml_file_path)?);
-    } else if cli.major {
-        bump_version(&cargo_toml_file_path, SemVer::Major)?;
-    } else if cli.minor {
-        bump_version(&cargo_toml_file_path, SemVer::Minor)?;
-    } else if cli.patch {
-        bump_version(&cargo_toml_file_path, SemVer::Patch)?;
-    } else {
-        // Safety: Either `version` contains a String supplied from the user or the CLI
-        // waits until it can read from stdin, in which case a version gets set as well.
-        set_version(&cargo_toml_file_path, cli.version.unwrap())?;
+    let res = match cli.command {
+        Commands::Get => {
+            let res = get_version(&cargo_toml_file_path);
+            if let Ok(version) = &res {
+                println!("{version}");
+            }
+            res
+        }
+        Commands::Set { mut version } => {
+            if let None = version {
+                version = read_stdin()?;
+            }
+            match version {
+                Some(v) => set_version(&cargo_toml_file_path, v),
+                None => Ok(Version::parse("0.0.0")?),
+            }
+        }
+        Commands::Major => bump_toml_version(&cargo_toml_file_path, Increment::Major),
+        Commands::Minor => bump_toml_version(&cargo_toml_file_path, Increment::Minor),
+        Commands::Patch => bump_toml_version(&cargo_toml_file_path, Increment::Patch),
+    };
+
+    if let Err(e) = res {
+        eprintln!("{e}");
+        exit(1);
     }
+
+    // // If no flag has been specified and no version, read from stdin.
+    // if !cli.major && !cli.minor && !cli.patch && !cli.get && cli.version.is_none() {
+    //     let mut piped = String::new();
+    //     io::stdin().read_line(&mut piped)?;
+    //     let piped_trim = piped.trim();
+    //     if !piped_trim.is_empty() {
+    //         cli.version = Some(piped_trim.to_string());
+    //     }
+    // }
+
+    // if cli.get {
+    //     println!("{}", get_version(&cargo_toml_file_path)?);
+    // } else if cli.major {
+    //     bump_toml_version(&cargo_toml_file_path, Increment::Major)?;
+    // } else if cli.minor {
+    //     bump_toml_version(&cargo_toml_file_path, Increment::Minor)?;
+    // } else if cli.patch {
+    //     bump_toml_version(&cargo_toml_file_path, Increment::Patch)?;
+    // } else {
+    //     // Safety: Either `version` contains a String supplied from the user or the CLI
+    //     // waits until it can read from stdin, in which case a version gets set as well.
+    //     set_version(&cargo_toml_file_path, cli.version.unwrap())?;
+    // }
 
     Ok(())
 }
